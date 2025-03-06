bc3cb5e6b816a71d9f27fabc3c360a0c0770aaaf
xramadhann <xxdhanz@gmail.com>
2025-03-06 20:28:07 +0700

    Refactor : change apk name

--- parkiram/lib/main.dart
+++ parkiram/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Parkiram());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Parkiram extends StatelessWidget {
+  const Parkiram({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
