9b42b0a4602d1488106e3cea50cac319c8ea0ce6
xramadhann <xxdhanz@gmail.com>
2025-03-13 02:39:30 +0700

    Refactor : Create ParkiramAvailable.dart

---
+++ parkiram/lib/View/ParkiramAvailable.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class ParkiramAvailable extends StatefulWidget {
+  const ParkiramAvailable({super.key});
+
+  @override
+  State<ParkiramAvailable> createState() => _ParkiramAvailableState();
+}
+
+class _ParkiramAvailableState extends State<ParkiramAvailable> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
