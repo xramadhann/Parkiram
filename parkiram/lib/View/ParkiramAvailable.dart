fa74a260ff2195682b98a92b05bb9849af1cc37d
xramadhann <xxdhanz@gmail.com>
2025-03-13 03:46:24 +0700

    Feat : Add static location Parkiram

--- parkiram/lib/View/ParkiramAvailable.dart
+++ parkiram/lib/View/ParkiramAvailable.dart
@@ -21,6 +21,39 @@ class _ParkiramAvailableState extends State<ParkiramAvailable> {
           alignment: Alignment.center,
           children: [
             Positioned(
+              top: 45,
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.9,
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                decoration: BoxDecoration(),
+                child: Column(
+                  children: const [
+                    Text(
+                      "Plaza Kalibata",
+                      style: TextStyle(
+                        fontSize: 36,
+                        fontWeight: FontWeight.w600,
+                        color: Color.fromARGB(255, 246, 255, 0),
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Jl. Raya Kalibata, Kec. Pancoran, Kota Jakarta Selatan, 12750",
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                        color: Color.fromARGB(255, 246, 255, 0),
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
               bottom: 0,
               left: 0,
               right: 0,
