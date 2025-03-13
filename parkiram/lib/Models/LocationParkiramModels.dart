78a97df27ba1d8127a3a600d13af4d5c78e96eb2
xramadhann <xxdhanz@gmail.com>
2025-03-14 04:54:00 +0700

    perf : replace data models from firebase realtime database

--- parkiram/lib/Models/LocationParkiramModels.dart
+++ parkiram/lib/Models/LocationParkiramModels.dart
@@ -1,15 +1,34 @@
-// ignore_for_file: file_names
+import 'package:parkiram/Models/FilterParkiramModels.dart';
 
 class ParkingModel {
   final String title;
   final String address;
   final String priceFirstHour;
   final String priceNextHour;
+  final List<VehicleType> vehicleTypes;
 
   ParkingModel({
     required this.title,
     required this.address,
     required this.priceFirstHour,
     required this.priceNextHour,
+    required this.vehicleTypes,
   });
+
+  // Factory method untuk membuat objek dari Firebase data snapshot
+  factory ParkingModel.fromMap(Map<dynamic, dynamic> data) {
+    return ParkingModel(
+      title: data['title'] ?? '',
+      address: data['address'] ?? '',
+      priceFirstHour: data['priceFirstHour'] ?? '',
+      priceNextHour: data['priceNextHour'] ?? '',
+      vehicleTypes: (data['vehicleTypes'] as List<dynamic>?)
+              ?.map((e) => VehicleType.values.firstWhere(
+                    (type) => type.toString().split('.').last == e,
+                    orElse: () => VehicleType.car,
+                  ))
+              .toList() ??
+          [],
+    );
+  }
 }
--- parkiram/lib/ViewModels/LocationParkiramViewModels.dart
+++ parkiram/lib/ViewModels/LocationParkiramViewModels.dart
@@ -1,62 +1,15 @@
-// ignore_for_file: file_names, unrelated_type_equality_checks
+// ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:parkiram/Models/FilterParkiramModels.dart';
-
-class ParkingModel {
-  final String title;
-  final String address;
-  final String priceFirstHour;
-  final String priceNextHour;
-  final List<VehicleType> vehicleTypes;
-
-  ParkingModel({
-    required this.title,
-    required this.address,
-    required this.priceFirstHour,
-    required this.priceNextHour,
-    required this.vehicleTypes,
-  });
-}
+import 'package:parkiram/Models/LocationParkiramModels.dart';
 
 class ParkingViewModel extends ChangeNotifier {
-  List<ParkingModel> _parkingList = [
-    ParkingModel(
-      title: "Kalibata City",
-      address: "Jl. Raya Kalibata, Kec. Pancoran, Kota Jakarta Selatan, 12750",
-      priceFirstHour: "Rp.5000",
-      priceNextHour: "Rp.1000",
-      vehicleTypes: [
-        VehicleType.car,
-        VehicleType.electricCar,
-        VehicleType.truck
-      ],
-    ),
-    ParkingModel(
-      title: "Basurra City",
-      address:
-          "Jl. Jenderal Basuki Rachmat, Kec. Jatinegara, Kota Jakarta Timur, 13410",
-      priceFirstHour: "Rp.3000",
-      priceNextHour: "Rp.1000",
-      vehicleTypes: [
-        VehicleType.car,
-        VehicleType.electricCar,
-        VehicleType.truck
-      ],
-    ),
-    ParkingModel(
-      title: "Plaza Kalibata",
-      address: "Jl. Raya Kalibata, Kec. Pancoran, Kota Jakarta Selatan, 12750",
-      priceFirstHour: "Rp.3000",
-      priceNextHour: "Rp.1000",
-      vehicleTypes: [
-        VehicleType.car,
-        VehicleType.electricCar,
-        VehicleType.truck
-      ],
-    ),
-  ];
+  final DatabaseReference _dbRef =
+      FirebaseDatabase.instance.ref(); // Ambil dari root
 
+  List<ParkingModel> _parkingList = [];
   List<ParkingModel> _filteredParkingList = [];
   String _searchQuery = "";
   VehicleType? _selectedFilter;
@@ -66,12 +19,28 @@ class ParkingViewModel extends ChangeNotifier {
   VehicleType? get selectedFilter => _selectedFilter;
 
   ParkingViewModel() {
-    applyFilter(null);
+    fetchParkingData();
   }
 
-  void setParkingList(List<ParkingModel> list) {
-    _parkingList = list;
-    applyFilter(_selectedFilter);
+  void fetchParkingData() {
+    _dbRef.onValue.listen((DatabaseEvent event) {
+      final data = event.snapshot.value;
+
+      if (data != null && data is Map<dynamic, dynamic>) {
+        _parkingList = data.entries.map((entry) {
+          debugPrint("Loading parking data: ${entry.key}"); // Debug Log
+          final parkingData = entry.value as Map<dynamic, dynamic>;
+          return ParkingModel.fromMap(parkingData);
+        }).toList();
+      } else {
+        debugPrint("No parking data found");
+        _parkingList = [];
+      }
+
+      applyFilter(_selectedFilter);
+    }, onError: (error) {
+      debugPrint("Error fetching parking data: $error");
+    });
   }
 
   void applyFilter(VehicleType? selectedFilter) {
