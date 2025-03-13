24c48f822fb005f0521cd9ec8058d6d41d6f86cc
xramadhann <xxdhanz@gmail.com>
2025-03-14 06:09:16 +0700

    Feat : create ParkiramAvailableViewModels.dart

---
+++ parkiram/lib/ViewModels/ParkiramAvailableViewModels.dart
@@ -0,0 +1,28 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+class ParkiramAvailableViewModel extends ChangeNotifier {
+  String availableSlot = "0"; // Data slot tersedia
+  late DatabaseReference _databaseRef;
+
+  ParkiramAvailableViewModel(String parkingTitle) {
+    _fetchData(parkingTitle);
+  }
+
+  void _fetchData(String parkingTitle) {
+    String parkingPath = parkingTitle.replaceAll(" ", "");
+    _databaseRef =
+        FirebaseDatabase.instance.ref("$parkingPath/parkingSlot/availableSlot");
+
+    _databaseRef.onValue.listen((DatabaseEvent event) {
+      if (event.snapshot.value != null) {
+        availableSlot = event.snapshot.value.toString();
+        notifyListeners(); // Notifikasi perubahan ke UI
+      }
+    }, onError: (error) {
+      debugPrint("Error mengambil data slot: $error");
+    });
+  }
+}
